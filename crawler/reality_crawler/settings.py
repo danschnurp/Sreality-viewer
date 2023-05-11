d2e7d805eab42b271361161a3c9eab854751b2a1
danschnurpfeil <dschnurp@email.cz>
2023-05-11 22:02:46 +0200

    adjustments for politeness

--- crawler/reality_crawler/settings.py
+++ crawler/reality_crawler/settings.py
@@ -9,7 +9,7 @@
 
 BOT_NAME = "reality_crawler"
 
-POSTGRES_URI = "postgresql://postgres:sreality@db:5433/sreality"
+POSTGRES_URI = "postgresql://postgres:sreality@db:5432/sreality"
 
 ITEM_PIPELINES = {
     'reality_crawler.pipelines.PostgresPipeline': 300,
@@ -23,7 +23,7 @@ NEWSPIDER_MODULE = "reality_crawler.spiders"
 #USER_AGENT = "reality_crawler (+http://www.yourdomain.com)"
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -31,7 +31,7 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 6
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
